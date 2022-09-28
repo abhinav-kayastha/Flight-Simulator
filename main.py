7789625db327e850a152880e3ab092f65a67c699
abhin <abhinavk@metropolia.fi>
2022-09-28 09:45:32 +0300

    mysql deletion

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import mysql.connector
-
 screen_name = input("Enter Player Name: ")
 plane_capacity = 200
 co2_budget = 1000
@@ -7,13 +5,6 @@ co2_consumed = 0
 location = "EFHK"
 
 
-connection = mysql.connector.connect(
-    host='127.0.0.1',
-    port=3306,
-    database='flight_game',
-    user='root',
-    password='2012004',
-    autocommit=True
-)
+
 
 
