c91aefa85de681e21a5c22b257f11f93706e8ffc
abhin <abhinavk@metropolia.fi>
2022-09-29 11:30:08 +0300

    get airport name function added and a non working passenger travel function added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-screen_name = input("Enter Player Name: ")
+import mysql.connector
+import random
 
-plane_capacity = 200
+screen_name = input("Enter Player Name: ")
 
 co2_budget = 1000
 co2_consumed = 0
@@ -8,8 +9,31 @@ co2_consumed = 0
 location = "EFHK"
 
 
+def getAirports():
+    sql = "SELECT name FROM airport WHERE iso_country = 'FI'" + f";"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
 
 
+def passengerTravel():
+    plane_capacity = 150
+    for airport in getAirports():
+        airport_passenger = random.randint(0, plane_capacity)
+        plane_capacity = plane_capacity - airport_passenger
+        airport_passenger = airport + ": " + str(airport_passenger)
+    return airport_passenger
 
 
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port="3306",
+    database="flight_game",
+    user="root",
+    password="2012004",
+    autocommit=True
+)
 
+print(getAirports())
+print(passengerTravel())
