d0e099e78d831a70635d2d98c8aa4543569fa395
abhin <abhinavk@metropolia.fi>
2022-12-12 12:20:56 +0200

    moving website files to their respective directories and removing the templates directory

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template("01_GamePage.html")
+@app.route('/user_data', methods=["GET", "POST"])
+def user_data():
+    if request == "POST":
+        username = request.form.get("username")
+        return "Your name is " + username
+    else:
+        return f"Did not find username."
 
 
 if __name__ == '__main__':
