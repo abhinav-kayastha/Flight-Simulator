d645510671d1a453ea189fe9b658512d3462466c
abhin <abhinavk@metropolia.fi>
2022-12-12 12:51:35 +0200

    finally connecting the frontend to the backend

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/user_data', methods=["GET", "POST"])
 def user_data():
-    if request == "POST":
-        username = request.form.get("username")
+    if request.method == "POST":
+        username = request.form.get("uname")
         return "Your name is " + username
     else:
         return f"Did not find username."
