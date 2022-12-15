34976e9c4711d07e9daf6f1c80948dacb9185481
abhin <abhinavk@metropolia.fi>
2022-12-15 17:21:37 +0200

    getting username

---
+++ static/username.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const username = prompt('Enter your username.')
--- static/username_prompt.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const username = prompt('Enter your username.')
-
-const p = document.getElementById('username')
-
-p.insertAdjacentText('afterend', `${username}`)
